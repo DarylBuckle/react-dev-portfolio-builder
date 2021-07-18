ce1a9d32378bc4e08abf9b4d2bca22e66b28a718
Daryl Buckle <darylbuckle1@gmail.com>
2021-07-18 21:06:54 +0100

    Updates

---
+++ precache-manifest.de7a971916455c2cfea5800f0bbbc46b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2395cc16d25a1953ee47695f875ea9b4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ca50dda938348d0aa350",
+    "url": "./static/css/2.5faddac7.chunk.css"
+  },
+  {
+    "revision": "128108900c01a12f9860",
+    "url": "./static/css/main.041f761d.chunk.css"
+  },
+  {
+    "revision": "ca50dda938348d0aa350",
+    "url": "./static/js/2.c7ced178.chunk.js"
+  },
+  {
+    "revision": "189688707f34b0f0dfeb233cdc689b4b",
+    "url": "./static/js/2.c7ced178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "128108900c01a12f9860",
+    "url": "./static/js/main.91b9cfe4.chunk.js"
+  },
+  {
+    "revision": "a9d40c5b77501db853147513981c863b",
+    "url": "./static/js/main.91b9cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12b878c97852c4b1b684",
+    "url": "./static/js/runtime-main.d0f1a815.js"
+  },
+  {
+    "revision": "d3ac5ac2355e178460b2342dcc41f51a",
+    "url": "./static/media/cover.d3ac5ac2.jpg"
+  }
+]);
